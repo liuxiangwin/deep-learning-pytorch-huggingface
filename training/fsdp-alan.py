44194a0437ca63876c0d20fcc725e2dc6a154364
liuxiangwin <liuxiangwni@163.com>
2024-12-08 16:37:55 +0800

    Add the training/fsdp-alan.ipynb and training/fsdp-alan.py for llm familiy tuning

--- training/fsdp-alan.py
+++ training/fsdp-alan.py
@@ -21,25 +21,45 @@ if torch.cuda.is_bf16_supported():
 else:
   compute_dtype = torch.float16
   attn_implementation = 'sdpa'
-# model_name = "meta-llama/Meta-Llama-3.1-70B"
-# model_name = "meta-llama/Llama-3.1-8B"
-model_name = "meta-llama/Llama-2-13b-hf"
 
-#Tokenizer
-tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
-tokenizer.pad_token = "<|finetune_right_pad_id|>"
-tokenizer.pad_token_id = 128004
+# model_name = "TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T"    #Pass
+# model_name = "meta-llama/Llama-3.1-8B"                                #Pass
+# model_name = "meta-llama/Llama-2-7b-hf"                               #Pass
+# model_name = "meta-llama/Llama-2-13b-hf"                              #Pass
+
+
+# model_name = "codellama/CodeLlama-34b-hf"
+# model_name = "meta-llama/Llama-2-70b-hf"
+# model_name = "meta-llama/Meta-Llama-3.1-70B" Not Pass
+
+
+
+#Tokenizer  Original Keep for initial
+# tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+# if tokenizer.pad_token is None:
+#     tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+
+# tokenizer.pad_token = "<|finetune_right_pad_id|>"
+# tokenizer.pad_token_id = 128004
+# tokenizer.padding_side = 'right'
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
 tokenizer.padding_side = 'right'
+
 ds = load_dataset("timdettmers/openassistant-guanaco")
+
 #Add the EOS token
 def process(row):
     row["text"] = row["text"]+"<|end_of_text|>"
     return row
+    
 ds = ds.map(
     process,
     num_proc= multiprocessing.cpu_count(),
     load_from_cache_file=False,
 )
+
 bnb_config = BitsAndBytesConfig(
         load_in_4bit=True,
         bnb_4bit_quant_type="nf4",
@@ -48,15 +68,20 @@ bnb_config = BitsAndBytesConfig(
         bnb_4bit_quant_storage=compute_dtype,
 )
 model = AutoModelForCausalLM.from_pretrained(
-          model_name, quantization_config=bnb_config, torch_dtype=torch.bfloat16, attn_implementation=attn_implementation
+          model_name,
+          quantization_config=bnb_config,
+          torch_dtype=torch.bfloat16,
+          attn_implementation=attn_implementation
 )
 for name, param in model.named_parameters():
     # freeze base model's layers
     param.requires_grad = False
+    
 def make_inputs_require_grad(module, input, output):
     output.requires_grad_(True)
 model.get_input_embeddings().register_forward_hook(make_inputs_require_grad)
 model.gradient_checkpointing_enable(gradient_checkpointing_kwargs={'use_reentrant':True})
+
 peft_config = LoraConfig(
         lora_alpha=16,
         lora_dropout=0.05,
